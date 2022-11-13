6fc1a933da162c863d35810b75cb6b1b0bc94d60
unknown <dbwp031@khu.ac.kr>
2022-11-13 20:28:45 +0900

    create yolox_l_woLoss.py

--- exps/example/custom/yolox_l_woLoss.py
+++ exps/example/custom/yolox_l_woLoss.py
@@ -9,14 +9,3 @@ class Exp(MyExp):
         self.depth = 1.0
         self.width = 1.0
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
-        
-    def get_model(self):
-        from yolox.models.yolo_pafpn import YOLOPAFPN
-        from yolox.models.yolo_head_woLoss import YOLOXHead
-        from yolox.models.yolox_woLoss import YOLOX
-        in_channels = [256, 512, 1024]
-        backbone = YOLOPAFPN(self.depth, self.width, in_channels=in_channels, act=self.act)
-        head = YOLOXHead(self.num_classes, self.width, in_channels=in_channels, act=self.act)
-        self.model = YOLOX(backbone, head)
-        return self.model
-    
