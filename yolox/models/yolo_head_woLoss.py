08ea346147127525362a060ae0575689e816e2d3
unknown <dbwp031@khu.ac.kr>
2022-11-13 20:13:41 +0900

    detach loss function from yolo_head

--- yolox/models/yolo_head_woLoss.py
+++ yolox/models/yolo_head_woLoss.py
@@ -163,26 +163,26 @@ class YOLOXHead(nn.Module):
 
             if self.training:
                 output = torch.cat([reg_output, obj_output, cls_output], 1)
-                output, grid = self.get_output_and_grid(
-                    output, k, stride_this_level, xin[0].type()
-                )
-                x_shifts.append(grid[:, :, 0])
-                y_shifts.append(grid[:, :, 1])
-                expanded_strides.append(
-                    torch.zeros(1, grid.shape[1])
-                    .fill_(stride_this_level)
-                    .type_as(xin[0])
-                )
-                if self.use_l1:
-                    batch_size = reg_output.shape[0]
-                    hsize, wsize = reg_output.shape[-2:]
-                    reg_output = reg_output.view(
-                        batch_size, self.n_anchors, 4, hsize, wsize
-                    )
-                    reg_output = reg_output.permute(0, 1, 3, 4, 2).reshape(
-                        batch_size, -1, 4
-                    )
-                    origin_preds.append(reg_output.clone())
+                # output, grid = self.get_output_and_grid(
+                #     output, k, stride_this_level, xin[0].type()
+                # )
+                # x_shifts.append(grid[:, :, 0])
+                # y_shifts.append(grid[:, :, 1])
+                # expanded_strides.append(
+                #     torch.zeros(1, grid.shape[1])
+                #     .fill_(stride_this_level)
+                #     .type_as(xin[0])
+                # )
+                # if self.use_l1:
+                #     batch_size = reg_output.shape[0]
+                #     hsize, wsize = reg_output.shape[-2:]
+                #     reg_output = reg_output.view(
+                #         batch_size, self.n_anchors, 4, hsize, wsize
+                #     )
+                #     reg_output = reg_output.permute(0, 1, 3, 4, 2).reshape(
+                #         batch_size, -1, 4
+                #     )
+                #     origin_preds.append(reg_output.clone())
 
             else:
                 output = torch.cat(
@@ -190,28 +190,28 @@ class YOLOXHead(nn.Module):
                 )
 
             outputs.append(output)
-
-        if self.training:
-            return self.get_losses(
-                imgs,
-                x_shifts,
-                y_shifts,
-                expanded_strides,
-                labels,
-                torch.cat(outputs, 1),
-                origin_preds,
-                dtype=xin[0].dtype,
-            )
-        else:
-            self.hw = [x.shape[-2:] for x in outputs]
-            # [batch, n_anchors_all, 85]
-            outputs = torch.cat(
-                [x.flatten(start_dim=2) for x in outputs], dim=2
-            ).permute(0, 2, 1)
-            if self.decode_in_inference:
-                return self.decode_outputs(outputs, dtype=xin[0].type())
-            else:
-                return outputs
+        return outputs
+        # if self.training:
+        #     return self.get_losses(
+        #         imgs,
+        #         x_shifts,
+        #         y_shifts,
+        #         expanded_strides,
+        #         labels,
+        #         torch.cat(outputs, 1),
+        #         origin_preds,
+        #         dtype=xin[0].dtype,
+        #     )
+        # else:
+        #     self.hw = [x.shape[-2:] for x in outputs]
+        #     # [batch, n_anchors_all, 85]
+        #     outputs = torch.cat(
+        #         [x.flatten(start_dim=2) for x in outputs], dim=2
+        #     ).permute(0, 2, 1)
+        #     if self.decode_in_inference:
+        #         return self.decode_outputs(outputs, dtype=xin[0].type())
+        #     else:
+        #         return outputs
 
     def get_output_and_grid(self, output, k, stride, dtype):
         grid = self.grids[k]
